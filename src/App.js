8ad63447fd1c5f938e1bab1e45323de82ec038a4
1x1x1x1cov <ongminhhieu12@gmail.com>
2023-12-17 10:44:57 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import { Navigate, Outlet } from "react-router-dom";
-import "./App.css";
-import SignUp from "./pages/Signup/SignUp";
-import useBearStore from "./state/state";
-import Header from "./UI/Header/Header";
-import Sidebar from "./UI/Sidebar/Sidebar";
+import React from 'react'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './PAGES/HomePage/Home'
+import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductPage from './PAGES/Product/ProductPage';
+import About from './PAGES/Extra/About';
+import Contact from './PAGES/Extra/Contact';
+import Login from './PAGES/Auth/Login';
+import Signup from './PAGES/Auth/Signup';
+import ForgotPassword from './PAGES/Auth/ForgotPassword';
+import Cart from './PAGES/Cart/Cart';
+import UserProfile from './PAGES/User/UserProfile';
+import FAQ from './PAGES/Extra/FAQ';
+import Termsandconditions from './PAGES/Extra/Termsandconditions';
+import PrivacyPolicy from './PAGES/Extra/PrivacyPolicy';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product/:prodid" 
+          element={
+            <ProductPage/>
+          }
+        />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/forgotpassword" element={<ForgotPassword/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path='/user/:activepage' element={<UserProfile/>} />
+        <Route path="/FAQ" element={<FAQ/>} />
+        <Route path="/termsandconditions" element={<Termsandconditions/>} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy/>} />
 
-function App() {
-  const isUserValid = useBearStore((state) => state.isUserValid);
-
-  // return <div>{isUserValid ? <Outlet /> : <Navigate to={"login"} />}</div>;
-
-  return isUserValid ? (
-    <div>
-      <Header />
-      <div className='d-flex'>
-        <Sidebar />
-        <div>
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  ) : (
-    <Navigate to={"/login"} />
-  );
+        <Route path="*" element={<div>
+          <h1>404 NOT FOUND</h1>
+        </div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
